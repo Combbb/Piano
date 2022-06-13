1cfac32932c297f711256c1d113c4913b9a07ff3
Combbb <107375632+Combbb@users.noreply.github.com>
2022-06-12 21:32:51 -0400

    Create Hello Adele.lua

---
+++ Hello Adele.lua
@@ -0,0 +1,101 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 285 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+
+shared.scr = [[
+[te60]| [tqe8]|||
+[ywr5]| [tqe4]|||
+df[te60]| [tqe8]|
+d d [ywr5]| [tqe4]|
+s f [tfe60] ff|[tqfe8]f|dd|
+sd[ywr5] fd|[tqpe4]s|
+sf f[te60] ds|[tqe8]|
+dddd[ywr5]| [tqe4]|
+f f ff[te60] ff|[tqfe8]f|d|
+ddd[ywrd5]|d|[tsqe4]|s|
+df[te60]| [tqe8]|
+d dd[ywr5] f| [tqe4]|
+s ff[te60] ff|[tqfe8]f|dd|
+sd[ywr5] fd|[tqpe4]s|
+ssf f[te60] ds|[tqe8]|
+d d [ywr5]| [tqe4]|
+s ff[te60] ff|[tqfe8]f|dd|
+sd[ywr5] fd p[tqe4]s|
+s s f [utfe6]| d [ywsr5]|||
+fh[wur3]|fgf[tied4]s|
+s f [utfe6]| d [ywrd5]| [tsqe4]|||
+[tpe60] p s [tsie4] d d d|
+[wut8]| [ywsr5]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]fd| [ywsr5]|
+sh ds[te60]|f|[tsie4]| s|
+sss[wuts8] a p [ywro5]|
+oo h s [te60] f| [tsie4]| s|
+[wuts8] a p [ywro5]| p|
+[tpe60] p s [tsie4] d| d|
+f[wutd8]| [ywsr5]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]fd| [ywsr5]| sh ds
+[te60]|f|[tsie4]| s|
+s[wuts8] a p [ywro5]|oo h s
+[tse60] f| [tsie4]| s|
+ss[wuts8] a p [ywro5]|
+o p yt[te60]| [tqe8]|||
+[ywr5]| [tqe4]|||
+df[te60]| [tqe8]|||
+dd[ywr5] d| [tqe4]|
+s f [tfe60] ff|[tqfe8]f|dd|
+sd[ywr5] fd|[tqpe4]
+s|p|df[te60]| [tqe8]|
+d d [ywrd5]| [tqe4]|
+sf f[te60]f f f[tqe8]f
+dd d sd[ywr5] fd p[tqe4]s
+p|s f [utfe6]| [ywrd5]|
+s h [wurh3]| f [tige4]|fds|
+f [utfe6]| d [ywrd5]| s [tsqe4]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]| [ywsr5]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]fd| [ywsr5]| sh ds
+[te60]|f|[tsie4]| s|
+sss[wuts8] a p [ywro5]|
+oo h s [te60] f| [tsie4]| s|
+[wuts8] a p [ywro5]| p|
+[tpe60] p s [tsie4] d| d|
+[wut8]| [ywsr5]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]fd| [ywsr5]|
+sh ds[te60]|f|[tsie4]| s|
+s[wuts8] a p [ywro5]|
+oo h s [tse60] f| [tsie4]| s|
+ss[wuts8] a p [ywro5]|
+p p p [utse6]|f|[tie4]|||
+h [ywr5]| gf[wut8]gf|
+sss[ute6]|f|[tie4]|||
+h [ywr5]| jh[wut8]l|
+sss[ute6]|f|[tie4]|||
+h [ywr5]| gf[wut8]gf|
+ddd[utpe6]ss| [tie4]|
+ddd[ywr5] f|||
+[tpe60] p s [tsie4] d d d|
+[wut8]| [ywsr5]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]fd| [ywsr5]|
+sh ds[te60]|f|[tsie4]| s|
+sss[wuts8] a p [ywro5]|
+oo h s [te60] f| [tsie4]| s|
+[wuts8] a p [ywro5]| p|
+[tpe60] p s [tsie4] d| d|f
+[wutd8]| [ywsr5]|||
+p [tpe60] p s [tsie4] d d d|
+[wut8]fd| [ywsr5]|
+sh ds[te60]|f|[tsie4]| s|
+s[wuts8] a p [ywro5]|
+oo h s [tse60] f| [tsie4]| s|
+ss[wuts8] a p [ywro5]|
+p p yt[ute6]| [wut8]|||
+[ywr5]| [tqe4]|||
+[te60]
+]]
