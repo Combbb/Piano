b18cbb92762b2903315e321a82f3e030762ff8f8
Combbb <107375632+Combbb@users.noreply.github.com>
2022-06-12 21:36:02 -0400

    Create Safe and Sound Taylor Swift.lua

---
+++ Safe and Sound Taylor Swift.lua
@@ -0,0 +1,66 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 229 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+
+shared.scr = [[
+[y9] p [gd]fy [e6] u [sp]|
+[oi4]p[pi] p o [y1]uuoeros[y9]
+p [gd]fy [e6] u [sp]|
+[pi4] [pi] s so[so1]| |
+[yf] yp[gd]fyo[se] u [sf] u o p p o yuuoerosy
+p [gd]fy e u [sp] o
+[po] i s [yi]o[ts] u [sf]|
+[yf] yp[gd]fyo[se] u [sf] u p p s o [yt]uuoerosw
+o [yd] yo[us] u d u p s d fp| |
+[sp4] i [sp] i [da] i [sf] i
+[sh8] o hfo [da5] o [da] o
+[sp4] i [sp] i [da] i [sf] i
+[sh8] o hfo [da5] o [da] o
+[sp4] i [sp] i [da] i [sf] i
+[sh8] o hfo [da7] o [da] o
+[sp6] u [sp] u [oa5] y [oa] y [tpi4]|||
+[fd9]gyp[gd]fyo[a6]su [sf] y
+[oi4]p[pi] [pi] [oi] [y1]uuoeros[y9]
+p [gd]fy [a6]su [sf]|
+[sp4] i [sp] yo[so1] u [sf] y
+[yf9] yp[gd]f[y9]o[se6] u [sf] u
+[o4] p p o [y8]uuo[e7]ros[y9] p [gd]fy [se6] u [sf] o
+[po4] i s [yi]o[ts8] u [sf] u
+[yf9] yp[gd]fyo[se6] u [sf] u
+[p4] p o o [y1]uo [so] uo[s5] y a|
+[p5]| d y 4|||4|||
+[sp4] i [sp] [i4] [da4] i [sf] i
+[sh8] o hf[o8] [da5] o [da] o
+[sp4] i [sp] [i4] [da4] i [sf] [i2]
+[sh1] o hf[o1] [da5] o [da] o
+[sp4] i [sp] [i4] [da4] i [sf] i
+[sh8] o hf[o8] [da5] o [da] o
+[sp6] u [sp] u [oa5] y [oa] y
+[spI$] [sp$] [sp$] [sp$] [sp$] [usp$] [usp$] [usp$]
+[spi4] [sp4] [sp4] [sf4] [pi4] [sp4] [da6] [sf4]
+[spo5] [sp5] [sp5] [sf5] [o5]p[s2] [d1] [f7]
+[pgd6] 6 [spf6] 6 [sp6] [sp6] [sp3] [sp6]
+[spg5] 5 [pfa5] 5 [spo5] [sp5] [sp5] [sp5]
+[spi4] [sp] [sp4] [sf4] [up4] 4 [sp6] 8
+[spo5] 5 [sf5] 5 [oa5] 5 [da5] 5
+[da6] 6 [sp6] 6 [upo8] [upo8] [spo6] [spo6]
+[spi5]| [uso]| 5| |
+p i [sp] ipd i f yo[th] u [sh] y [rd]| |
+[sp] i [sp] i [da] i [sf] yo[th] u hfuo[rd]| |
+[sp4] i [sp] i [da] i [sf] i
+[sh8] o hfo [da7] o [da] o
+[sp6] u [sp] u [oa5] y [oa] y [sp$]|||
+[sp4] i [sp4] i [sp] i [sp] i
+[sp8] u8[da7] o [da5] o [pd] o
+[sp4] i [sp] [i4] [sp4] i [sp] i
+[so8] u [da] [o5] [da5] o [pf] o
+[sp4] i [sp] [i4] [sp4] i [sp] i
+[so8] u [da] [o8] [da5] o [sp] u
+[sp4] i [sp] [i4] [sp4] i [sp] i
+[so8] u [da] [o8] [da5] o [sp] u
+[sp4] i [sp] [i4] [sp4] [i5] [sp6] [i4]
+[so8] u [da] [o8] [da5] o [sp] u
+[sp4]|||[uso8]| |[hda5]| [spf]
+]]
